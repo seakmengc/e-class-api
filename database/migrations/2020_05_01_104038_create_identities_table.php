ad382b0aad7845e142967a918c5b22be923c28ee
SeakmengChheang <c.seakmeng0603@gmail.com>
2020-05-08 19:42:33 +0700

    added: id in identity

--- database/migrations/2020_05_01_104038_create_identities_table.php
+++ database/migrations/2020_05_01_104038_create_identities_table.php
@@ -14,6 +14,8 @@ class CreateIdentitiesTable extends Migration
     public function up()
     {
         Schema::create('identities', function (Blueprint $table) {
+            $table->id();
+
             $table->foreignId('user_id')
                 ->constrained()
                 ->onDelete('cascade');
