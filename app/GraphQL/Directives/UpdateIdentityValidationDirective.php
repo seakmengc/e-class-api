b90fbcd2a05719cf5b8ef3d3e9c7b4d213f16b62
SeakmengChheang <c.seakmeng0603@gmail.com>
2020-05-11 01:12:11 +0700

    [FEATURE] updateIdentity

---
+++ app/GraphQL/Directives/UpdateIdentityValidationDirective.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\GraphQL\Directives;
+
+use Nuwave\Lighthouse\Schema\Directives\ValidationDirective;
+
+class UpdateIdentityValidationDirective extends ValidationDirective
+{
+    /**
+     * @return mixed[]
+     */
+    public function rules(): array
+    {
+        return [
+            'contact_number' => 'max:255',
+            'first_name' => 'max:255',
+            'last_name' => 'max:255',
+            'gender' => 'in:male,female,others',
+            'photo' => 'image|max:5120'
+        ];
+    }
+}
