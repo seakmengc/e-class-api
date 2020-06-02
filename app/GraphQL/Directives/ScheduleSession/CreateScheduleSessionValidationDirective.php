322d5dd6f7baeb9f43abc1d62e8851cf026cfe4c
Piseth <piseth@pop-os.localdomain>
2020-06-02 14:42:37 +0700

    [FEATURES] schedule and schedule sessions

---
+++ app/GraphQL/Directives/ScheduleSession/CreateScheduleSessionValidationDirective.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\GraphQL\Directives\ScheduleSession;
+
+use Illuminate\Support\Str;
+use Illuminate\Validation\Rule;
+use Nuwave\Lighthouse\Schema\Directives\ValidationDirective;
+
+class CreateScheduleSessionValidationDirective extends ValidationDirective
+{
+
+  public function rules(): array
+  {
+    return [
+      'start_date' => 'required|digits:4',
+      'end_date' => 'required|digits:4',
+    ];
+  }
+}
