69b48519e4e1ec1aba00816b296c6b608bea3086
SeakmengChheang <c.seakmeng0603@gmail.com>
2020-06-15 21:14:43 +0700

    add: mark notification as read

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -9,21 +9,35 @@ use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
 
 class Notification extends Model
 {
+
     protected $fillable = ['type', 'notifiable_type', 'notifiable_id', 'data'];
 
+    protected $keyType = 'string';
+
     protected $casts = [
-        'data' => 'collection',
-        'read_at' => 'datetime'
+        'data' => 'collection'
     ];
 
     public function isRead()
     {
-        return (bool) $this->read_at;
+        $rtn = (bool) $this->read_at;
+
+        $this->markAsRead();
+
+        return $rtn;
     }
 
     public function paginate($root, array $args, GraphQLContext $context, ResolveInfo $resolveInfo): Builder
     {
         return Notification::where('notifiable_id', $context->user->id)
-            ->where('notifiable_type', get_class($context->user));
+            ->where('notifiable_type', get_class($context->user))
+            ->latest();
+    }
+
+    public function markAsRead()
+    {
+        if (is_null($this->read_at)) {
+            $this->forceFill(['read_at' => $this->freshTimestamp()])->save();
+        }
     }
 }
