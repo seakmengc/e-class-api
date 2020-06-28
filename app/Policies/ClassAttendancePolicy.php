c0c0a2e5062c69fde3f871798fe71e0c01733f9f
SeakmengChheang <c.seakmeng0603@gmail.com>
2020-06-29 01:28:27 +0700

    update: class attendance policy

--- app/Policies/ClassAttendancePolicy.php
+++ app/Policies/ClassAttendancePolicy.php
@@ -8,50 +8,46 @@ use Illuminate\Auth\Access\HandlesAuthorization;
 
 class ClassAttendancePolicy
 {
-    use HandlesAuthorization;
+	use HandlesAuthorization;
 
-     public function view(User $user)
-     {
-        //
-        return $user->isTeacher() || $user->isAdmin();
-     }
+	public function view(User $user, ClassAttendance $classAttendance)
+	{
+		return $user->isATeacherOf($classAttendance->class_id);
+	}
 
-    /**
-     * Determine whether the user can create models.
-     *
-     * @param  User  $user
-     * @return mixed
-     */
-    public function create(User $user)
-    {
-      return $user->isTeacher();
-    }
+	/**
+	 * Determine whether the user can create models.
+	 *
+	 * @param  User  $user
+	 * @return mixed
+	 */
+	public function create(User $user, $injected)
+	{
+		return $user->isATeacherOf($injected['class_id']);
+	}
 
-    /**
-     * Determine whether the user can update the model.
-     *
-     * @param  User  $user
-     * @param  ClassAttendance  $classAttendance
-     * @return mixed
-     */
-    public function update(User $user, ClassAttendance $classAttendance)
-    {
-        //
-        return $user->isTeacher() && $user->isATeacherOf($classAttendance->class_id);
-    }
+	/**
+	 * Determine whether the user can update the model.
+	 *
+	 * @param  User  $user
+	 * @param  ClassAttendance  $classAttendance
+	 * @return mixed
+	 */
+	public function update(User $user, ClassAttendance $classAttendance)
+	{
+		return $user->isATeacherOf($classAttendance->class_id);
+	}
 
-    /**
-     * Determine whether the user can delete the model.
-     *
-     * @param  User  $user
-     * @param  ClassAttendance  $classAttendance
-     * @return mixed
-     */
-    public function delete(User $user, ClassAttendance $classAttendance)
-    {
-        //
-          return $user->isTeacher() && $user->isATeacherOf($classAttendance->class_id);
-    }
-
-
+	/**
+	 * Determine whether the user can delete the model.
+	 *
+	 * @param  User  $user
+	 * @param  ClassAttendance  $classAttendance
+	 * @return mixed
+	 */
+	public function delete(User $user, ClassAttendance $classAttendance)
+	{
+		//
+		return $user->isATeacherOf($classAttendance->class_id);
+	}
 }
