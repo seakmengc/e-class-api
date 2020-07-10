bf8ffba3592ba3bd5430f28ff99335a709eb9e20
SeakmengChheang <c.seakmeng0603@gmail.com>
2020-07-11 02:49:34 +0700

    fix

--- app/Models/ClassCategory.php
+++ app/Models/ClassCategory.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Facades\DB;
 
 class ClassCategory extends Model
 {
@@ -17,7 +18,7 @@ class ClassCategory extends Model
 
 	public function exams(): HasMany
 	{
-		if (auth()->id() == $this->class->teacher_id) {
+		if (auth()->id() == DB::table('classes')->whereId($this->attributes['class_id'])->pluck('teacher_id')->first()) {
 			return $this->hasMany(Exam::class);
 		}
 
